1b6a48a81bf3870af1128a8fd9310c8462429642
Tricia Leavitt <leavitt.tricia@gmail.com>
2018-03-17 19:24:31 -0700

    html routes established

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const express = require ('express');
 const app = express(); 
 const bodyParser = require('body-parser'); 
 const path = require('path'); 
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
