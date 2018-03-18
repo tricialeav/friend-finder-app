0a59f1772c39435916908753bee2f4dc96df504e
Tricia Leavitt <leavitt.tricia@gmail.com>
2018-03-18 14:23:07 -0700

    remove node_modules, add gitignore, misc work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,12 @@
 // You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-    // ```json
-    // {
-    //   "name":"Ahmed",
-    //   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    //   "scores":[
-    //       5,
-    //       1,
-    //       4,
-    //       4,
-    //       5,
-    //       1,
-    //       2,
-    //       5,
-    //       4,
-    //       1
-    //     ]
-    // }
-    // ```
+    var friendsArray = [
+        {
+          name: "Ahmed",
+          photo: "ahmed@example.com",
+          scores: [3, 2, 2, 4, 1, 3, 4, 5, 2, 5]
+        }
+      ];
+      
+      // Note how we export the array. This makes it accessible to other files using require.
+      module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,29 @@
 // Your `apiRoutes.js` file should contain two routes:
     // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
     // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
+const friendData = require("../data/friends");
+
+module.exports = function(app) {
+  
+    app.get("/api/friends", function(req, res) {
+      res.json(friendsData);
+    });
+  
+    app.post("/api/friends", function(req, res) {
+      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+      // It will do this by sending out the value "true" have a table
+      // req.body is available since we're using the body-parser middleware
+      if (friendsData.length < 5) {
+        friendsData.push(req.body);
+        res.json(true);
+      }
+    });
+  
+    app.post("/api/clear", function() {
+      friendsData = [];
+  
+      console.log(friendsData);
+    });
+  };
+  
