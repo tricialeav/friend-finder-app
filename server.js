ac7afc2468707275fbd747f703badb3f125f0a90
Tricia Leavitt <leavitt.tricia@gmail.com>
2018-03-17 19:03:34 -0700

    planning stage

---
+++ server.js
@@ -0,0 +1,6 @@
+// Your `server.js` file should require the basic npm packages we've used in class: `express`, `body-parser` and `path`.
+
+const express = require ('express'); 
+const app = express(); 
+const bodyParser = require('body-parser'); 
+const path = require('path'); 
