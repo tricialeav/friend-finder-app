46924f0160569eb470ff457e6dd3dd98bf77a4ab
Tricia Leavitt <leavitt.tricia@gmail.com>
2018-03-18 20:04:50 -0700

    app now takes in user data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,23 +7,23 @@ const friendData = require("../data/friends");
 module.exports = function(app) {
   
     app.get("/api/friends", function(req, res) {
-      res.json(friendsData);
+      res.json(friendData);
     });
   
     app.post("/api/friends", function(req, res) {
       // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
       // It will do this by sending out the value "true" have a table
       // req.body is available since we're using the body-parser middleware
-      if (friendsData.length < 5) {
-        friendsData.push(req.body);
+      if (friendData.length < 5) {
+        friendData.push(req.body);
         res.json(true);
       }
     });
   
     app.post("/api/clear", function() {
-      friendsData = [];
+      friendData = [];
   
-      console.log(friendsData);
+      console.log(friendData);
     });
   };
   
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
